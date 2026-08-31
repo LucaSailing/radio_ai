38dd28cc5edc0ad97e0241cf0fb6b09606286b33
Merwan Bekka <merwanbekka@gmail.com>
2026-08-31 12:28:08 +0200

    model uptade metrics

--- radio_ai_package/ml_logic/models/model_CNN.py
+++ radio_ai_package/ml_logic/models/model_CNN.py
@@ -149,6 +149,7 @@ def train_model(model, ds_train, ds_val, y_train=None):
         #class_weight=class_weight,
         callbacks=cbs,
         verbose=1
+        metrics = ['loss', 'accuracy', 'recall']
     )
     return model, history
 
